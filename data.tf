df122a7a8ef051119dff3d0b0d10af290b46aa2f
Henrique Martelete <henriquemartelete@Henriques-MacBook-Pro.local>
2023-08-04 23:08:46 +0100

    Updating files

--- data.tf
+++ data.tf
@@ -4,11 +4,10 @@ data "aws_region" "current" {}
 ### In case you want to update the AMI to the latest
 data "aws_ami" "amazon_linux" {
   most_recent = true
-
+  owners = ["amazon"]
+  
   filter {
     name   = "name"
-    values = ["amzn-ami-hvm-*-x86_64-gp*"]
+    values = ["amzn2-ami-hvm*"]
   }
-
-  owners = ["amazon"] # Canonical
 }
